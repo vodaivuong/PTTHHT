fd52dcc644a064c761221dec5548bd03ff250f41
VoDaiVuong <80749685+vodaivuong@users.noreply.github.com>
2021-09-14 18:47:33 +0700

    Create InChuoi.java

---
+++ javaBasic/InChuoi.java
@@ -0,0 +1,10 @@
+package javaBasic;
+
+public class InChuoi {
+    public static void main(String[] args) {
+        String s = "Chao mung";
+        int i = 123;
+        
+        System.out.println(s + " " + i);
+    }
+}
