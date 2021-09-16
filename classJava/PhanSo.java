95911cc6f5e95dde937c0b01da4aa37730e28aee
VoDaiVuong <80749685+vodaivuong@users.noreply.github.com>
2021-09-16 21:19:21 +0700

    Update PhanSo.java

--- classJava/PhanSo.java
+++ classJava/PhanSo.java
@@ -33,9 +33,14 @@ public class PhanSo {
 
     //Nghich dao phan so
     void nghichDao(){
-        
+        int tuc, mauc;
+        tuc = getMau();
+        mauc = getTu();
+        setTu(tuc);
+        setMau(mauc);
     }
-    
+
+    //Tim uoc chung lon nhat
     public int timUSCLN(int a, int b) {
         while (a != b) {
             if (a > b) {
@@ -46,19 +51,18 @@ public class PhanSo {
         }
         return a;
     }
+
     //Toi gian phan so
     public void toiGianPhanSo() {
         int i = timUSCLN(this.getTu(), this.getMau());
         this.setTu(this.getTu() / i);
         this.setMau(this.getMau() / i);
     }
+
     //Tra ve gia tri thuc cua phan so
-    // public double giaTRiThuc(){
-    //     PhanSo x;
-    //     double gt;
-    //     gt = x.tu / x.mau;
-    //     return gt;
-    // }
+    public float giaTRiThuc(){
+        return (float)getTu() / (float)getMau();
+    }
 
     //Cong 2 phan so
     public void congPhanSo(PhanSo ps) {
@@ -68,6 +72,7 @@ public class PhanSo {
         phanSoTong.toiGianPhanSo();
         System.out.println("Tong 2 phan so = " + phanSoTong.tu + "/" + phanSoTong.mau);
     }
+
     //Tru 2 phan so
     public void truPhanSo(PhanSo ps) {
         int ts = this.getTu() * ps.getMau() - ps.getTu() * this.getMau();
@@ -76,6 +81,7 @@ public class PhanSo {
         phanSoHieu.toiGianPhanSo();
         System.out.println("Hieu 2 phan so = " + phanSoHieu.tu + "/" + phanSoHieu.mau);
     }
+
     //Nhan 2 phan so
     public void nhanPhanSo(PhanSo ps) {
         int ts = this.getTu() * ps.getTu();
@@ -84,6 +90,7 @@ public class PhanSo {
         phanSoTich.toiGianPhanSo();
         System.out.println("Tich 2 phan so = " + phanSoTich.tu + "/" + phanSoTich.mau);
     }
+
     //Chia 2 phan so
     public void chiaPhanSo(PhanSo ps) {
         int ts = this.getTu() * ps.getMau();
@@ -94,8 +101,13 @@ public class PhanSo {
     }
 
     public static void main(String[] args) {
-        PhanSo ps1 = new PhanSo(3,9);
-        PhanSo ps2 = new PhanSo(2,8);
+        PhanSo ps1 = new PhanSo(3,7);
+        PhanSo ps2 = new PhanSo(2,9);
+
+        //In man hinh phan so da nhap vao
+        System.out.println("Phan so da nha vao: ");
+        ps1.inManHinh();
+        ps2.inManHinh();
 
         //Toi gian phan so
         ps1.toiGianPhanSo();
@@ -105,19 +117,21 @@ public class PhanSo {
         ps1.inManHinh();
         ps2.inManHinh();
 
-        //Gia tri thuc cua phan so
-        // System.out.println("Gia tri thuc cua ps1 = " + ps1.giaTRiThuc());
-        // System.out.println("Gia tri thuc cua ps1 = " + ps2.giaTRiThuc());
-
         // Cong tru nhan chia 2 phan so
         ps1.congPhanSo(ps2);
         ps1.truPhanSo(ps2);
         ps1.nhanPhanSo(ps2);
         ps1.chiaPhanSo(ps2);
 
-        //
-        // ps1.nghichDao();
-        // System.out.println("Phan so sau khi nghich dao: ");
-        // ps1.inManHinh();
+        // Gia tri thuc cua phan so
+        System.out.println("Gia tri thuc cua ps1 = " + ps1.giaTRiThuc());
+        System.out.println("Gia tri thuc cua ps1 = " + ps2.giaTRiThuc());
+
+        //In ra nghich dao
+        ps1.nghichDao();
+        ps2.nghichDao();
+        System.out.println("Phan so sau khi nghich dao: ");
+        ps1.inManHinh();
+        ps2.inManHinh();
     }
 }
