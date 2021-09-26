ae7ffc486c4e41f07add6c2965f880cdb11a08da
VoDaiVuong <80749685+vodaivuong@users.noreply.github.com>
2021-09-26 18:14:49 +0700

    Update TestSleep.java

--- javaThread/TestSleep.java
+++ javaThread/TestSleep.java
@@ -1,22 +1,22 @@
 public class TestSleep extends Thread{
     // Truong hop su dung Sleep
-    // public void run() {
-    //     for (int i = 0; i < 10; i++) {
-    //         try {
-    //             Thread.sleep(2000);
-    //         } catch (Exception e) {
-    //             System.out.println(e);
-    //         }
-    //         System.out.println("Luong dang chay " + i);
-    //     }
-    // }
-
-    // Truong hop khong su dung khong Sleep
     public void run() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
             System.out.println("Luong dang chay " + i);
         }
     }
+
+    // Truong hop khong su dung khong Sleep
+    // public void run() {
+    //     for (int i = 0; i < 5; i++) {
+    //         System.out.println("Luong dang chay " + i);
+    //     }
+    // }
     public static void main(String[] args) {
         TestSleep t1 = new TestSleep();
         TestSleep t2 = new TestSleep();
