1e8820ed1b06cf03028a7e8711e23f9a62ff613d
VoDaiVuong <80749685+vodaivuong@users.noreply.github.com>
2021-09-26 18:14:43 +0700

    Update TestMultiNaming.java

--- javaThread/TestMultiNaming.java
+++ javaThread/TestMultiNaming.java
@@ -1,28 +1,28 @@
 public class TestMultiNaming extends Thread{
-    // public void run() {
-    //     System.out.println("Running...");
-    // }
-    // public static void main(String[] args) {
-    //     TestMultiNaming t1 = new TestMultiNaming();
-    //     TestMultiNaming t2 = new TestMultiNaming();
-    //     System.out.println("Name of t1: " + t1.getName());
-    //     System.out.println("Name of t2: " + t2.getName());
-
-    //     t1.start();
-    //     t2.start();
-
-    //     t1.setName("Auto Find Image");
-    //     System.out.println("After changing name of t1: " + t1.getName());
-    // }
-
     public void run() {
-        System.out.println(Thread.currentThread().getName());
+        System.out.println("Running...");
     }
     public static void main(String[] args) {
         TestMultiNaming t1 = new TestMultiNaming();
         TestMultiNaming t2 = new TestMultiNaming();
+        System.out.println("Name of t1: " + t1.getName());
+        System.out.println("Name of t2: " + t2.getName());
 
         t1.start();
         t2.start();
+
+        t1.setName("Auto Find Image");
+        System.out.println("After changing name of t1: " + t1.getName());
     }
+
+    // public void run() {
+    //     System.out.println(Thread.currentThread().getName());
+    // }
+    // public static void main(String[] args) {
+    //     TestMultiNaming t1 = new TestMultiNaming();
+    //     TestMultiNaming t2 = new TestMultiNaming();
+
+    //     t1.start();
+    //     t2.start();
+    // }
 }
